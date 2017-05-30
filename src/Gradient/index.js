6593afa568993951e7642f8ee027e2e476acbee3
Ignacio Martin <ignacio.m@medicalwebexperts.com>
2017-05-30 17:35:51 -0300

    modified gradient component

--- src/Gradient/index.js
+++ src/Gradient/index.js
@@ -7,24 +7,27 @@ import styles from './styles';
 const propTypes = {
   children: PropTypes.node,
   gradientColors: PropTypes.array.isRequired,
+  locations: PropTypes.array.isRequired,
   style: PropTypes.object,
 };
 
 const defaultProps = {
   children: undefined,
   gradientColors: [],
+  locations: [0.3, 0.6, 1],
   style: {},
 };
 
+// locations has to have the same amount as colors, if not a warning is raised
 
-function Gradient({ children, gradientColors, style }) {
+function Gradient({ children, gradientColors, locations, style }) {
   const stylesCommon = style ? StyleSheet.flatten([styles, style]) : styles;
   const gradient = (
     <LinearGradient
       colors={gradientColors}
       start={{ x: 0, y: 1 }}
       end={{ x: 0.8, y: 1 }}
-      locations={[0.3, 0.6, 1]}
+      locations={locations}
       style={stylesCommon.container}
     >
       {children}
