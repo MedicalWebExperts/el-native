1d827b7721f1917ac5694bf1bfe289706a71ddad
Lionel Cuevas <lionel.c@medicalwebexperts.com>
2017-11-09 19:00:18 -0300

    removed legacy components

---
+++ dist/index.js
@@ -0,0 +1,4 @@
+var _Map=require('./Map');var _Map2=_interopRequireDefault(_Map);function _interopRequireDefault(obj){return obj&&obj.__esModule?obj:{default:obj};}
+
+module.exports={
+Map:_Map2.default};
--- index.js
+++
@@ -1,3 +0,0 @@
-export { default as Gradient } from './src/Gradient';
-export { default as ImageSwiper } from './src/ImageSwiper';
-export { default as Map } from './src/Map';
