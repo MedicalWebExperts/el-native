e241da0ee323952227af4236fff8ade95e3e2a08
Martin Carrera <mcarrera.92@gmail.com>
2018-01-18 15:45:32 -0300

    Refactor proptypes and defaultprops

--- storybook/stories/CenterView/index.js
+++ storybook/stories/CenterView/index.js
@@ -3,14 +3,18 @@ import { oneOfType, number, object } from 'prop-types';
 import { View } from 'react-native';
 import style from './style';
 
-const CenterView = props => <View style={[style.main, props.style]}>{props.children}</View>;
-CenterView.propTypes = {
+const propTypes = {
   children: oneOfType([number, object]),
   style: oneOfType([number, object]),
 };
-CenterView.defaultProps = {
+
+const defaultProps = {
   children: null,
   style: {},
 };
+
+const CenterView = props => <View style={[style.main, props.style]}>{props.children}</View>;
+CenterView.propTypes = propTypes;
+CenterView.defaultProps = defaultProps;
 
 export default CenterView;
