9ed876fb0a9b9a4decb62648774821bdafc57576
Martin Carrera <mcarrera.92@gmail.com>
2018-01-19 12:25:54 -0300

    Improve generator

--- generate-component.sh
+++ generate-component.sh
@@ -1,11 +1,23 @@
 #!/bin/bash
+if [ -z "$1" ]; then
+	echo "You need to add the new module name."
+	return
+fi
+MODULE=$1
+if [ -d "src/${MODULE}" ]; then
+	echo "The module ${MODULE} already exists."
+	return
+fi
 
 echo "Creating component folder and files..."
 
-mkdir src/$1
-touch src/$1/$1.js
-touch src/$1/$1.test.js
-mkdir storybook/stories/$1
-touch storybook/stories/$1/index.js
+# Make directories
+mkdir src/${MODULE}
+mkdir storybook/stories/${MODULE}
+
+# Create files
+sed "s/<module>/${MODULE}/g" "templates/component" > "src/${MODULE}/${MODULE}.js"
+sed "s/<module>/${MODULE}/g" "templates/test" > "src/${MODULE}/${MODULE}.test.js"
+sed "s/<module>/${MODULE}/g" "templates/story" > "storybook/stories/${MODULE}/index.js"
 
 echo "Finished"
