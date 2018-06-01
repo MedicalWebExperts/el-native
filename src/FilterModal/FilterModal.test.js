5ccc8a4fc271ef7a6586afeb973b46891a68c83a
Ignacio Martin <ignacio.m@medicalwebexperts.com>
2018-06-01 13:20:24 -0300

    improved snapshot

--- src/FilterModal/FilterModal.test.js
+++ src/FilterModal/FilterModal.test.js
@@ -3,9 +3,24 @@ import renderer from 'react-test-renderer';
 
 import FilterModal from './FilterModal';
 
+const filters = [
+  { name: 'Pediatrics', value: true },
+  { name: 'Plastic Surgery', value: false },
+  { name: 'Gynecologist', value: false },
+  { name: 'Cardiac Surgery', value: false },
+];
+
+const props = {
+  modalVisible: false,
+  title: 'Specialties',
+  filters,
+  updateFilter: () => null,
+  closeModal: () => null,
+};
+
 describe('FilterModal Snapshots', () => {
   it('should render a simple FilterModal', () => {
-    const component = renderer.create(<FilterModal />);
+    const component = renderer.create(<FilterModal {...props} />);
     const json = component.toJSON();
     expect(json).toMatchSnapshot();
   });
