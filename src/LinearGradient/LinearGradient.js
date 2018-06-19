dcd1941928f10a84e09d6a929c6426ce0c78abf7
leandro.c <leandro.c@medicalwebexperts.com>
2018-06-19 17:33:03 -0300

    add proptypes

--- src/LinearGradient/LinearGradient.js
+++ src/LinearGradient/LinearGradient.js
@@ -1,6 +1,14 @@
 import React from 'react';
 
 import LinearGradient from 'react-native-linear-gradient';
+import { node } from 'prop-types';
+
+const PropTypes = {
+  children: node.isRequired,
+};
 
 const LG = props => <LinearGradient {...props}>{props.children}</LinearGradient>;
+
+LG.propTypes = PropTypes;
+
 export default LG;
