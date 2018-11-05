2c232094f473eae68ab8089463aab6817b64688d
Ignacio Martin <ignacio.m@medicalwebexperts.com>
2018-11-05 15:26:42 -0300

    removed traces of linear gradient

--- src/LinearGradient/LinearGradient.js
+++ src/LinearGradient/LinearGradient.js
@@ -1,13 +1,14 @@
 import React from 'react';
-
-import LinearGradient from 'react-native-linear-gradient';
+import { View } from 'react-native';
+// import LinearGradient from 'react-native-linear-gradient';
 import { node } from 'prop-types';
 
 const PropTypes = {
   children: node.isRequired,
 };
 
-const LG = props => <LinearGradient {...props}>{props.children}</LinearGradient>;
+const LG = props => <View {...props}>{props.children}</View>;
+// const LG = props => <LinearGradient {...props}>{props.children}</LinearGradient>;
 
 LG.propTypes = PropTypes;
 
